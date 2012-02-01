b72129e17e9163e0fa60e8fc4fe69dd301a8cf56
Dell <timger@ubuntu.(none)>
2012-02-01 14:13:31 +0800

    sasa

---
+++ install_page.sh
@@ -0,0 +1,20 @@
+sudo apt-get install vim
+sudo apt-get install vim
+sudo apt-get install mysql
+sudo apt-get install python2.7-dev
+sudo apt-get install mysql-server
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo easy_install mysqldb
+sudo easy_install MySQL-python
+sudo apt-get install mysql
+sudo apt-get install mysql-client
+sudo easy_install MySQL-python
+sudo easy_install mysql-python
+sudo easy_install sqlalchemy
+sudo apt-get install nginx
+sudo apt-get install libmysqld-dev
+sudo apt-get install libmysqlclient-dev 
+sudo easy_install mysql-python
+sudo easy_install lxml
+sudo apt-get install libxml2
--- vim_config/vim/template/myjs.js
+++ vim_config/vim/template/myjs.js
@@ -1,10 +1,7 @@
 /**
  * @author timger <yishenggudou@gmail.com>
  * @timger http://weibo.com/zhanghaibo
-<<<<<<< HEAD
  * twitter @yishenggudou http://twitter.com/yishenggudou
-=======
  * @yishenggudou http://twitter.com/yishenggudou
->>>>>>> 5df8223977276112aaedafd1e98444995cd47a87
  * Copyright (c) 2008-2011 timger - released under MIT License {{{
  */
